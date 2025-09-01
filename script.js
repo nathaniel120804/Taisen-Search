89b07d7ac2d6eea07ba9cc8465cd49e8adca29c2
nathaniel120804 <hybeentertainment722@gmail.com>
2025-09-01 18:52:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-// Extra JS if you want to do something after search is ready
 window.onload = () => {
   console.log("Google Custom Search Engine loaded!");
+
+  // Initialize search element
+  google.search.cse.element.render({
+    div: "searchbox",
+    tag: 'searchbox-only',
+    attributes: {
+      enableAutoComplete: true
+    }
+  });
+
+  // Listen for search events
+  google.search.cse.element.getElement('searchbox').execute = (query) => {
+    console.log("User searched:", query);
+
+    // You can send this to your server to store
+    fetch('/log-search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query })
+    });
+
+    // Call the original execute to perform search
+    google.search.cse.element.getElement('searchbox').originalExecute(query);
+  };
 };
